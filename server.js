5a764942feb0f803dff89ece6e30f92ea3c8d77c
rwmitch <rwmitch@protonmail.com>
2018-10-25 13:26:24 +0100

    Guild specific settings

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.config = require("./config.json");
 const fs = require("file-system");
+const Enmap = require("enmap");
+client.guildConfs = new Enmap({
+  name: "settings",
+  autoFetch: true,
+  fetchAll: false
+});
+
+client.on("guildCreate", guild => {
+  client.guildConfs.set(guild.id, client.config.defaultSettings);
+});
 
 client.on("ready", () => {
   console.log("Discord bot up");
@@ -11,14 +21,14 @@ client.on("message", msg => {
   client.args = msg.content.slice(client.config.prefix.length).trim().split(/ +/g);
   client.cmd = client.args.shift().toLowerCase();
 
-  if (msg.content.startsWith(client.config.prefix) && msg.guild != null) {
-    if (client.config.cmds.includes(client.cmd)) {
-      var cmdFile = require("cmds/" + client.cmd + ".js");
-      cmdFile.run(client, msg);
-    } else {
-      var cmdFile = require("cmds/404.js");
-      cmdFile.run(client, msg);
-    }
+  if (!msg.content.startsWith(client.config.prefix) || !msg.guild || msg.author.bot) return;
+
+  if (client.config.cmds.includes(client.cmd)) {
+    var cmdFile = require("cmds/" + client.cmd + ".js");
+    cmdFile.run(client, msg);
+  } else {
+    var cmdFile = require("cmds/404.js");
+    cmdFile.run(client, msg);
   }
 });
 
